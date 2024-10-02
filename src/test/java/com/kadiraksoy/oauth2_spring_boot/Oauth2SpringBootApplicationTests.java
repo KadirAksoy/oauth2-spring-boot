ab2a866b84192bd8e0291db7804b72017e0f5af4
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2024-10-02 15:57:29 +0300

    Initial commit

---
+++ src/test/java/com/kadiraksoy/oauth2_spring_boot/Oauth2SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiraksoy.oauth2_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
